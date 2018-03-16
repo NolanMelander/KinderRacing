3dad8957da8bbc5940bedf11fb920937e64ad3dc
Nolan Melander <mel09001@byui.edu>
2018-03-16 09:42:52 -0600

    Initial Commits

---
+++ Interface/track.py
@@ -0,0 +1,13 @@
+import pygame
+
+#LAP TRACKER
+lap_1 = pygame.image.load("..\Resources\Laps\lap_1.png")
+lap_2 = pygame.image.load("..\Resources\Laps\lap_2.png")
+lap_3 = pygame.image.load("..\Resources\Laps\lap_3.png")
+lap_4 = pygame.image.load("..\Resources\Laps\lap_4.png")
+lap_5 = pygame.image.load("..\Resources\Laps\lap_5.png")
+lap_6 = pygame.image.load("..\Resources\Laps\lap_6.png")
+lap_7 = pygame.image.load("..\Resources\Laps\lap_7.png")
+lap_8 = pygame.image.load("..\Resources\Laps\lap_8.png")
+lap_9 = pygame.image.load("..\Resources\Laps\lap_9.png")
+lap_10 = pygame.image.load("..\Resources\Laps\lap_10.png")
