fa185e5502ff903804228d3bc6a4240dea630eff
Nolan Melander <mel09001@byui.edu>
2018-03-16 09:43:59 -0600

    Added Lap Track At Top Of UI

--- Interface/game.py
+++ Interface/game.py
@@ -1,18 +1,16 @@
 import pygame
 import Interface.cars as cars
+import Interface.track as track
+
 from random import randint
 
 pygame.init()
 
 # SETUP THE GAME WINDOW
-#display_width, display_height = pygame.display.Info().current_w, pygame.display.Info().current_h
-#gameDisplay = pygame.display.set_mode((display_width, display_height))
-
 display_width, display_height = 800, 800
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Kinder Racing')
 
-black = (0, 0, 0)
 
 clock = pygame.time.Clock()
 raceFinished = False
@@ -36,6 +34,43 @@ def computer_three_car(x, y):
     gameDisplay.blit(cars.computer_three, (x, y))
 
 
+def lap_tracker(lap):
+    if lap == 1:
+        image = pygame.image.load("..\Resources\Laps\lap_1.png")
+    elif lap == 2:
+        image = pygame.image.load("..\Resources\Laps\lap_2.png")
+    elif lap == 3:
+        image = pygame.image.load("..\Resources\Laps\lap_3.png")
+    elif lap == 4:
+        image = pygame.image.load("..\Resources\Laps\lap_4.png")
+    elif lap == 5:
+        image = pygame.image.load("..\Resources\Laps\lap_5.png")
+    elif lap == 6:
+        image = pygame.image.load("..\Resources\Laps\lap_6.png")
+    elif lap == 7:
+        image = pygame.image.load("..\Resources\Laps\lap_7.png")
+    elif lap == 8:
+        image = pygame.image.load("..\Resources\Laps\lap_8.png")
+    elif lap == 9:
+        image = pygame.image.load("..\Resources\Laps\lap_9.png")
+    elif lap == 10:
+        image = pygame.image.load("..\Resources\Laps\lap_10.png")
+
+    image = pygame.transform.scale(image, (50, 50))
+    return image
+
+
+def display_lap(pc, comp_1, comp_2, comp_3):
+    gameDisplay.blit(pc, (115, 0))
+    gameDisplay.blit(comp_1, (300, 0))
+    gameDisplay.blit(comp_2, (500, 0))
+    gameDisplay.blit(comp_3, (700, 0))
+    player_car(0, 0)
+    computer_one_car(175, 0)
+    computer_two_car(375, 0)
+    computer_three_car(575, 0)
+    pass
+
 # CARS SETUP
 player_x = comp_1_x = comp_2_x = comp_3_x = 0
 player_y = 150
@@ -46,10 +81,10 @@ player_x_change = comp_1_change = comp_2_change = comp_3_change = 0
 
 
 # TRACK LAPS
-player_laps = 0
-comp_1_laps = 0
-comp_2_laps = 0
-comp_3_laps = 0
+player_laps = 1
+comp_1_laps = 1
+comp_2_laps = 1
+comp_3_laps = 1
 
 while not raceFinished:
     for event in pygame.event.get():
@@ -75,7 +110,11 @@ while not raceFinished:
 
     # PREPARE DISPLAY
     gameDisplay.blit(background, (0, 95, display_width, display_height))
-    #gameDisplay.fill(black)
+    player_lapImg = lap_tracker(player_laps)
+    comp_1_lapImg = lap_tracker(comp_1_laps)
+    comp_2_lapImg = lap_tracker(comp_2_laps)
+    comp_3_lapImg = lap_tracker(comp_3_laps)
+    display_lap(player_lapImg, comp_1_lapImg, comp_2_lapImg, comp_3_lapImg)
     player_car(player_x, player_y)
     computer_one_car(comp_1_x, comp_1_y)
     computer_two_car(comp_2_x, comp_2_y)
@@ -87,22 +126,22 @@ while not raceFinished:
     if player_x > display_width:
         player_x = -80
         player_laps += 1
-        print("Orange has completed lap " + (str(player_laps)))
+        print("Orange is on lap " + (str(player_laps)))
 
     if comp_1_x > display_width:
         comp_1_x = -80
         comp_1_laps += 1
-        print("Black has completed lap " + (str(comp_1_laps)))
+        print("Black is on lap " + (str(comp_1_laps)))
 
     if comp_2_x > display_width:
         comp_2_x = -80
         comp_2_laps += 1
-        print("Red has completed lap " + (str(comp_2_laps)))
+        print("Red is on lap " + (str(comp_2_laps)))
 
     if comp_3_x > display_width:
         comp_3_x = -80
         comp_3_laps += 1
-        print("White has completed lap " + (str(comp_1_laps)))
+        print("White is on " + (str(comp_1_laps)))
 
 pygame.quit()
 quit
