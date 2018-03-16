4e2e7cadbe9fb267522c7cc060fe03c8f50d227e
Nolan Melander <mel09001@byui.edu>
2018-03-16 13:45:43 -0600

    Initial Letters

---
+++ Interface/letters.py
@@ -0,0 +1,134 @@
+import pygame
+
+
+# LETTERS
+def set_letter_a():
+    letter_a = pygame.image.load("..\Resources\Letters\A.png")
+    return letter_a
+
+
+def set_letter_b():
+    letter_b = pygame.image.load("..\Resources\Letters\B.png")
+    return letter_b
+
+
+def set_letter_c():
+    letter_c = pygame.image.load("..\Resources\Letters\C.png")
+    return letter_c
+
+
+def set_letter_d():
+    letter_d = pygame.image.load("..\Resources\Letters\D.png")
+    return letter_d
+
+
+def set_letter_e():
+    letter_e = pygame.image.load("..\Resources\Letters\E.png")
+    return letter_e
+
+
+def set_letter_f():
+    letter_f = pygame.image.load("..\Resources\Letters\F.png")
+    return letter_f
+
+
+def set_letter_g():
+    letter_g = pygame.image.load("..\Resources\Letters\G.png")
+    return letter_g
+
+
+def set_letter_h():
+    letter_h = pygame.image.load("..\Resources\Letters\H.png")
+    return letter_h
+
+
+def set_letter_i():
+    letter_i = pygame.image.load("..\Resources\Letters\I.png")
+    return letter_i
+
+
+def set_letter_j():
+    letter_j = pygame.image.load("..\Resources\Letters\J.png")
+    return letter_j
+
+
+def set_letter_k():
+    letter_k = pygame.image.load("..\Resources\Letters\K.png")
+    return letter_k
+
+
+def set_letter_l():
+    letter_l = pygame.image.load("..\Resources\Letters\L.png")
+    return letter_l
+
+
+def set_letter_m():
+    letter_m = pygame.image.load("..\Resources\Letters\M.png")
+    return letter_m
+
+
+def set_letter_n():
+    letter_n = pygame.image.load("..\Resources\Letters\\N.png")
+    return letter_n
+
+
+def set_letter_o():
+    letter_o = pygame.image.load("..\Resources\Letters\O.png")
+    return letter_o
+
+
+def set_letter_p():
+    letter_p = pygame.image.load("..\Resources\Letters\P.png")
+    return letter_p
+
+
+def set_letter_q():
+    letter_q = pygame.image.load("..\Resources\Letters\Q.png")
+    return letter_q
+
+
+def set_letter_r():
+    letter_r = pygame.image.load("..\Resources\Letters\R.png")
+    return letter_r
+
+
+def set_letter_s():
+    letter_s = pygame.image.load("..\Resources\Letters\S.png")
+    return letter_s
+
+
+def set_letter_t():
+    letter_t = pygame.image.load("..\Resources\Letters\T.png")
+    return letter_t
+
+
+def set_letter_u():
+    letter_u = pygame.image.load("..\Resources\Letters\\U.png")
+    return letter_u
+
+
+def set_letter_v():
+    letter_v = pygame.image.load("..\Resources\Letters\V.png")
+    return letter_v
+
+
+def set_letter_w():
+    letter_w = pygame.image.load("..\Resources\Letters\W.png")
+    return letter_w
+
+
+def set_letter_x():
+    letter_x = pygame.image.load("..\Resources\Letters\X.png")
+    return letter_x
+
+
+def set_letter_y():
+    letter_y = pygame.image.load("..\Resources\Letters\Y.png")
+    return letter_y
+
+
+def set_letter_z():
+    letter_z = pygame.image.load("..\Resources\Letters\Z.png")
+    return letter_z
+
+
